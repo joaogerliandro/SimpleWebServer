ae90d6e94bce49af6720aeca1458850737e6d036
João Gerliandro <joaogerliandro@gmail.com>
2023-11-05 09:26:29 -0300

    refactor - little changes in project

---
+++ include/webserver.h
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <common.h>
+
+using boost::asio::ip::tcp;
+
+namespace WebServer
+{
+    class RequestHandler
+    {
+        public:
+            static void process_request(const std::string& request, tcp::socket *socket)
+            {
+                boost::system::error_code ignored_error;
+
+                if (request.find("GET") != std::string::npos) 
+                {
+                    size_t first_space = request.find(' ');
+                    size_t second_space = request.find(' ', first_space + 1);
+                    std::string requested_file = request.substr(first_space + 1, second_space - first_space - 1);
+                    
+                    if (requested_file == "/") 
+                    {
+                        requested_file = "index.html";
+                    }
+                    else
+                        requested_file = requested_file.substr(1);
+
+                    std::string response = generate_response(requested_file);
+                    boost::asio::write(*socket, boost::asio::buffer(response), ignored_error);
+                }
+            }
+
+        private:
+            static std::string generate_response(const std::string& filename) 
+            {
+                std::ifstream file(filename, std::ios::binary);
+                if (!file.is_open()) 
+                {
+                    return "HTTP/1.1 404 Not Found\r\n\r\nFile Not Found";
+                }
+
+                std::string file_extension = filename.substr(filename.find_last_of(".") + 1);
+                std::string content_type = map_content_type(file_extension);
+
+                std::string response = "HTTP/1.1 200 OK\r\n";
+                response += "Content-Type: " + content_type + "\r\n\r\n";
+                std::string file_content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+                response += file_content;
+
+                return response;
+            }
+
+            static std::string map_content_type(const std::string& file_extension) 
+            {
+                if (file_extension == "html") 
+                {
+                    return "text/html";
+                } 
+                else if (file_extension == "css") 
+                {
+                    return "text/css";
+                } 
+                else if (file_extension == "js") 
+                {
+                    return "application/javascript";
+                } 
+                else if (file_extension == "jpg" || file_extension == "jpeg") 
+                {
+                    return "image/jpeg";
+                } 
+                else if (file_extension == "png") 
+                {
+                    return "image/png";
+                } 
+                else if (file_extension == "gif") 
+                {
+                    return "image/gif";
+                }
+                else if (file_extension == "svg") 
+                {
+                    return "image/svg+xml";
+                }
+
+                return "application/octet-stream";
+            }
+    };
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,6 @@
-#include <common.h>
+#include <webserver.h>
 
 using boost::asio::ip::tcp;
-
-std::string map_content_type(const std::string& file_extension) 
-{
-    if (file_extension == "html") 
-    {
-        return "text/html";
-    } 
-    else if (file_extension == "css") 
-    {
-        return "text/css";
-    } 
-    else if (file_extension == "js") 
-    {
-        return "application/javascript";
-    } 
-    else if (file_extension == "jpg" || file_extension == "jpeg") 
-    {
-        return "image/jpeg";
-    } 
-    else if (file_extension == "png") 
-    {
-        return "image/png";
-    } 
-    else if (file_extension == "gif") 
-    {
-        return "image/gif";
-    }
-    else if (file_extension == "svg") 
-    {
-        return "image/svg+xml";
-    }
-
-    return "application/octet-stream";
-}
-
-std::string generate_response(const std::string& filename) {
-    std::ifstream file(filename, std::ios::binary);
-    if (!file.is_open()) 
-    {
-        return "HTTP/1.1 404 Not Found\r\n\r\nFile Not Found";
-    }
-
-    std::string file_extension = filename.substr(filename.find_last_of(".") + 1);
-    std::string content_type = map_content_type(file_extension);
-
-    std::string response = "HTTP/1.1 200 OK\r\n";
-    response += "Content-Type: " + content_type + "\r\n\r\n";
-    std::string file_content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-    response += file_content;
-
-    return response;
-}
 
 int main()
 {
@@ -66,8 +14,6 @@ int main()
             tcp::socket *socket = new tcp::socket(io_service);
 
             acceptor.accept(*socket);
-
-            boost::system::error_code ignored_error;
 
             boost::asio::streambuf request;
 
@@ -85,22 +31,7 @@ int main()
             std::string request_line;
             std::getline(request_stream, request_line);
 
-            if (request_line.find("GET") != std::string::npos) 
-            {
-                size_t first_space = request_line.find(' ');
-                size_t second_space = request_line.find(' ', first_space + 1);
-                std::string requested_file = request_line.substr(first_space + 1, second_space - first_space - 1);
-                
-                if (requested_file == "/") 
-                {
-                    requested_file = "index.html";
-                }
-                else
-                    requested_file = requested_file.substr(1);
-
-                std::string response = generate_response(requested_file);
-                boost::asio::write(*socket, boost::asio::buffer(response), ignored_error);
-            }
+            WebServer::RequestHandler::process_request(request_line, socket);
 
             socket->close();
         }
